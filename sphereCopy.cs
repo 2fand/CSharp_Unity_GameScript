25fc21f8280b68a67c8680e22d8426219d557d6e
2fand <167662034+2fand@users.noreply.github.com>
2025-05-18 18:38:04 +0800

    Create sphereCopy.cs

---
+++ sphereCopy.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class sphereCopy : MonoBehaviour
+{
+    public GameObject o;
+    public int a = 11;
+    public float splita = 2.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (0 == a % 2)
+        {
+            a--;
+        }
+        o.GetComponent<setParent>().self = gameObject;
+        //int add = 0;
+        for (int x = -a / 2; x <= a / 2; x++) {
+            for (int y = -a / 2; y <= a / 2; y++)
+            {
+                for (int z = -a / 2; z <= a / 2; z++)
+                {
+                    if (x * x + y * y + z * z <= a * a / 4)
+                    {
+                        Instantiate(o, gameObject.transform.position + new Vector3(x * splita, y * splita, z * splita), Quaternion.Euler(Vector3.zero));
+                    }
+                }
+            }
+        }
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+    //1 2 3 4 5 6 7
+    // Update is called once per frame
+    
+
+
+    
+}
