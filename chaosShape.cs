f4393d9270eeba81f718510bd135048faf9a4f65
2fand <167662034+2fand@users.noreply.github.com>
2025-05-19 19:52:41 +0800

    创建C#脚本chaosShape.cs。

---
+++ chaosShape.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chaosShape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] vector3s = GetComponent<MeshFilter>().mesh.vertices;
+        for (int i = 0; i < 24; i++)
+        {
+            vector3s[i] = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+        }
+        GetComponent<MeshFilter>().mesh.SetVertices(vector3s);
+        GetComponent<MeshFilter>().mesh.RecalculateNormals();
+    }
+}
