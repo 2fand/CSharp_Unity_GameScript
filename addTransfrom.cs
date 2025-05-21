5239a6ed8ad38580b978f6012c175e8605564fe4
2fand <167662034+2fand@users.noreply.github.com>
2025-05-21 20:08:38 +0800

    创建addTransfrom.csC#脚本文件

---
+++ addTransfrom.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addTransfrom : MonoBehaviour
+{
+    public Vector3 addPos;
+    public Vector3 addRotation;
+    public Vector3 addScale;
+    public Transform thisAddTrans;
+    public bool secondAdd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (null != thisAddTrans)
+        {
+            transform.position += thisAddTrans.position * (secondAdd ? Time.deltaTime : 1);
+            transform.Rotate(thisAddTrans.rotation.eulerAngles * (secondAdd ? Time.deltaTime : 1));
+            transform.localScale += thisAddTrans.localScale * (secondAdd ? Time.deltaTime : 1);
+        }
+        else
+        {
+            transform.position += addPos * (secondAdd ? Time.deltaTime : 1);
+            transform.Rotate(addRotation * (secondAdd ? Time.deltaTime : 1));
+            transform.localScale += addScale * (secondAdd ? Time.deltaTime : 1);
+        }
+    }
+}
