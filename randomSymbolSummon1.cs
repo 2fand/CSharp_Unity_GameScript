7c2609f9a81a75d989d8c23d7157256cef1f194f
2fand <167662034+2fand@users.noreply.github.com>
2025-05-25 17:35:10 +0800

    更新C#脚本randomSymbolSummon1.cs

--- randomSymbolSummon1.cs
+++ randomSymbolSummon1.cs
@@ -4,73 +4,42 @@ using UnityEngine;
 using Unity.Collections;
 using Unity.VisualScripting;
 using System.Runtime.CompilerServices;
+using TMPro;
+using System.Net;
+using System.Net.Sockets;
 
 public class randomSymbolSummon1 : MonoBehaviour
 {
-    public float space = 10;
-    public Material lineMaterial;
-    public bool isChange = true;
     public float waitTime = 0.1f;
     private bool isEnd = true;
-
-    private void summon()
-    {
-        Vector3[] dots = { new Vector3(0, 0, 0), new Vector3(space, 0, 0), new Vector3(2 * space, 0, 0), new Vector3(0, -space, 0), new Vector3(space, -space, 0), new Vector3(2 * space, -space, 0), new Vector3(0, -2 * space, 0), new Vector3(space, -2 * space, 0), new Vector3(2 * space, -2 * space, 0), new Vector3(0, -3 * space, 0), new Vector3(space, -3 * space, 0), new Vector3(2 * space, -3 * space, 0) };
-        for (int doti = 0; doti < 12; doti++)
-        {
-            dots[doti] += transform.position;
-        }
-        List<Vector3> paths = new List<Vector3>();
-        bool[] canMove = new bool[8];
-        int[] modeMove = { -4, -3, -2, -1, 1, 2, 3, 4 };
-        short dotuse = 0b100000000000;
-        int pos = 0;
-        int mode = 0;
-        while (0b111111111111 != dotuse)
-        {
-            paths.Add(dots[pos]);
-            canMove[1] = (0 != pos / 3);
-            canMove[3] = (0 != pos % 3);
-            canMove[4] = (2 != pos % 3);
-            canMove[6] = (3 != pos / 3);
-            canMove[0] = (canMove[1] && canMove[3]);
-            canMove[2] = (canMove[1] && canMove[4]);
-            canMove[5] = (canMove[6] && canMove[3]);
-            canMove[7] = (canMove[6] && canMove[4]);
-            mode = Random.Range(0, 8);
-            while (!canMove[mode])
-            {
-                mode = Random.Range(0, 8);
-            }
-            pos += modeMove[mode];
-            dotuse |= (short)(1 << pos);
-        }
-        GetComponent<LineRenderer>().positionCount = paths.Count;
-        GetComponent<LineRenderer>().SetPositions(paths.ToArray());
-    }
+    private char[] chars;
     IEnumerator write()
     {
         isEnd = false;
+        GetComponent<TextMeshPro>().text = chars[Random.Range(0, chars.Length)].ToString();
         yield return new WaitForSeconds(waitTime);
-        summon();
         isEnd = true;
     }
     // Start is called before the first frame update
     void Start()
     {
-        space *= space < 0 ? -1 : 1;
-        if (null == GetComponent<LineRenderer>())
+        if (null == GetComponent<TextMeshPro>())
         {
-            transform.AddComponent<LineRenderer>();
-            GetComponent<LineRenderer>().material = lineMaterial;
+            Debug.LogError("错误：没有TextMeshPro组件");
         }
-        summon();
+        chars = new char[127 - 33];
+        char ch = (char)33;
+        for (; ch < 127; ch++)
+        {
+            chars[ch - 33] = ch;
+        }
+        write();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isEnd && isChange)
+        if (isEnd)
         {
             StartCoroutine(write());
         }
