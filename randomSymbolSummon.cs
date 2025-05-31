a2bd20ae6af2a2d3a0583e80d20825625350f70a
2fand <167662034+2fand@users.noreply.github.com>
2025-05-31 17:11:08 +0800

    更新C#脚本randomSymbolSummon.cs

--- randomSymbolSummon.cs
+++ randomSymbolSummon.cs
@@ -13,12 +13,21 @@ public class randomSymbolSummon : MonoBehaviour
     public Material lineMaterial;
     public float waitTime = 0.1f;
     private bool isEnd = true;
+    public bool summonOneShot = false;
+    
+    private bool check(bool[] dotMove)
+    {
+        foreach (bool b in dotMove)
+        {
+            if (!b)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     private void summon()
     {
-        if (32 <= a + b) {
-            a = 3;
-            b = 4;
-        }
         Vector3[] dots = new Vector3[a * b];
         for (int doti = 0; doti < dots.Length; ++doti)
         {
@@ -32,12 +41,12 @@ public class randomSymbolSummon : MonoBehaviour
             modeMove[modei] = -step;
             modeMove[modeMove.Length - 1 - modei] = step--;
         }
-        int dotuse = 0;
+        bool[] dotMove = new bool[a + b];
         int pos = Random.Range(0, (int)(a * b));
         int mode = 0;
         bool LastDraw = 1 == Random.Range(0, 2);
         paths.Add(dots[pos]);
-        while (dotuse != (1 << (int)(a + b)) - 1 || LastDraw)
+        while (!check(dotMove) || LastDraw)
         {
             canMove[1] = (0 != pos / a);
             canMove[3] = (0 != pos % a);
@@ -47,7 +56,7 @@ public class randomSymbolSummon : MonoBehaviour
             canMove[2] = (canMove[1] && canMove[4]);
             canMove[5] = (canMove[6] && canMove[3]);
             canMove[7] = (canMove[6] && canMove[4]);
-            if (dotuse == (1 << (int)(a + b)) - 1)
+            if (check(dotMove))
             {
                 canMove[7 - mode] = false;
                 LastDraw = false;
@@ -58,7 +67,8 @@ public class randomSymbolSummon : MonoBehaviour
                 mode = Random.Range(0, 8);
             }
             pos += modeMove[mode];
-            dotuse |= (1 << (pos / (int)a)) + (1 << (int)(b + pos % a));
+            dotMove[pos % a] = true;
+            dotMove[a + pos / a] = true;
             paths.Add(dots[pos]);
         }
         GetComponent<LineRenderer>().positionCount = paths.Count;
@@ -85,16 +95,16 @@ public class randomSymbolSummon : MonoBehaviour
         {
             transform.AddComponent<LineRenderer>();
             GetComponent<LineRenderer>().material = lineMaterial;
-            GetComponent<LineRenderer>().useWorldSpace = false;
             GetComponent<LineRenderer>().startWidth = GetComponent<LineRenderer>().endWidth = 0.1f;
         }
+        GetComponent<LineRenderer>().useWorldSpace = false;
         summon();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isEnd)
+        if (!summonOneShot && isEnd)
         {
             StartCoroutine(write());
         }
