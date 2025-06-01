17475b484faa288b47605e38df62bf532dbebfd1
2fand <167662034+2fand@users.noreply.github.com>
2025-06-01 15:32:02 +0800

    Update addTransform.cs

--- addTransform.cs
+++ addTransform.cs
@@ -10,13 +10,11 @@ public class addTransfrom : MonoBehaviour
     public Vector3 addScale;
     public Transform thisAddTrans;
     public bool secondAdd;
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (null != thisAddTrans)
