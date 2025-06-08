fd6ffe973c3f5b08ef3cd26e01378075771885bf
2fand <167662034+2fand@users.noreply.github.com>
2025-06-08 17:40:07 +0800

    更新C#脚本fly.cs

--- fly/fly.cs
+++ fly/fly.cs
@@ -3,53 +3,51 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class fly : MonoBehaviour
 {
     public GameObject o;
-    public float r = 0.5f;
+    public float a = 0.5f;
     public float summonTime = 0.1f;
-    public float speed = 0.618f;
-    public float dieTime = 0.16f;
+    public float speed = 0.0618f;
+    public float dieTime = 2f;
     private bool isEnd = true;
     IEnumerator shot()
     {
         isEnd = false;
         yield return new WaitForSeconds(summonTime);
-        Instantiate(o, this.transform.position, Quaternion.Euler(Vector3.zero));
+        Instantiate(o, this.transform.position, o.transform.rotation);
         isEnd = true;
     }
 
     void Start()
     {
-        if (null == o.GetComponent<addTransform>())
+        if (null == o.GetComponent<particleRun>())
         {
-            o.transform.AddComponent<addTransform>();
-        }
-        if (null == o.GetComponent<die>())
-        {
-            o.transform.AddComponent<die>();
+            o.transform.AddComponent<particleRun>();
         }
     }
+
 
     void Update()
     {
         if (isEnd)
         {
-            if (r > 1){
-                r = 1;
-            }
-            if (r < 0){
-                r = 0;
-            }
-            float x = UnityEngine.Random.Range(-r, r);
-            float y = UnityEngine.Random.Range(-r, r);
-            if (x * x + y * y <= 1)
+            if (a < 0)
             {
-                o.GetComponent<addTransform>().addPos = new Vector3(x, Mathf.Sqrt(1 - x * x - y * y), y) * speed;
-                o.GetComponent<die>().dieTime = dieTime;
-                StartCoroutine(shot());
+                a = 0;
             }
+            if (a > 1)
+            {
+                a = 1;
+            }
+            float x = UnityEngine.Random.Range(-a, a);
+            float y = UnityEngine.Random.Range(-(Mathf.Sqrt(1 - x * x) < a ? Mathf.Sqrt(1 - x * x) : a), Mathf.Sqrt(1 - x * x) < a ? Mathf.Sqrt(1 - x * x) : a);
+            Vector3 spinPos = transform.rotation * new Vector3(x, y, Mathf.Sqrt(1 - x * x - y * y));
+            o.GetComponent<particleRun>().direction = spinPos * speed;
+            o.GetComponent<particleRun>().dieTime = dieTime;
+            StartCoroutine(shot());
         }
     }
 }
