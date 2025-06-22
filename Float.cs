8255ae7c53013d84ec0713e0ab8bd512ed968c66
2fand <167662034+2fand@users.noreply.github.com>
2025-06-22 13:50:27 +0800

    更新C#脚本Float.cs

--- Float.cs
+++ Float.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Float : MonoBehaviour
 {
-    private float f = -6.28f;
-    public float limit = 10;
-    public float y = 0;
+    public float speed = 1;
+    public float high = 1;
+    private float x;
+    public float ox;
+    public float oy;
+    public float oz;
     void Start()
     {
-
+        x = -3.14f / speed;
+        ox = transform.position.x;
+        oy = transform.position.y;
+        oz = transform.position.z;
     }
 
     void Update()
     {
-        this.transform.position = new Vector3(transform.position.x, Mathf.Cos(f / 2.0f) * limit + y, transform.position.z);
-        f += 0.01f;
-        if (f > 6.28f)
+        transform.position = new Vector3(ox, oy + Mathf.Sin(x * speed) * high, oz);
+        x += 0.01f;
+        if (x > 3.14f / speed)
         {
-            f = -6.28f;
+            x = -3.14f / speed;
         }
     }
 }
