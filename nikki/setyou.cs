3eaafc5e5488d7d27c671740f7f8dd202447d5cd
2fand <167662034+2fand@users.noreply.github.com>
2025-07-11 15:51:49 +0800

    创建C#脚本setyou.cs

---
+++ nikki/setyou.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setyou : MonoBehaviour
+{
+    public AudioClip defaultWalkSound;
+    public AudioClip effectEqiupSound;
+    public AudioClip effectCancelEqiupSound;
+    public Material[] screens;
+    public GameObject[] effects;
+    public AudioClip[] effectWalkSounds;
+    void Awake()
+    {
+        you.defaultWalkSound = defaultWalkSound ?? you.defaultWalkSound;
+        you.effectEqiupSound = effectEqiupSound ?? you.effectEqiupSound;
+        you.effectCancelEqiupSound = effectCancelEqiupSound ?? you.effectCancelEqiupSound;
+        you.screens = screens ?? you.screens;
+        you.effects = effects ?? you.effects;
+        you.effectWalkSounds = effectWalkSounds ?? you.effectWalkSounds;
+    }
+
+    void Update()
+    {
+        
+    }
+}
