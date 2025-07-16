00f5d60182e1abe39c96dc873870dcbdb8fdbb3e
2fand <167662034+2fand@users.noreply.github.com>
2025-07-16 19:31:24 +0800

    更新C#脚本summonWall.cs

--- nikki/summonWall.cs
+++ nikki/summonWall.cs
@@ -1,29 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class summonWall : MonoBehaviour
 {
+    public map m;
     public Vector2[] wallXY;
-    public Vector2 xyOffSet = Vector2.zero;//Æ«ÒÆÁ¿£¬¿ÉÒÔ´´½¨³ö±È1*1»¹´óµÄÇ½£¬Ò²¿ÉÒÔ¶ÔÉú³É³öÀ´µÄÇ½½øĞĞÕûÌåÒÆ¶¯
+    public string wallXYstr;
+    public Vector2 xyOffSet = Vector2.zero;//åç§»é‡ï¼Œå¯ä»¥åˆ›å»ºå‡ºæ¯”1*1è¿˜å¤§çš„å¢™ï¼Œä¹Ÿå¯ä»¥å¯¹ç”Ÿæˆå‡ºæ¥çš„å¢™è¿›è¡Œæ•´ä½“ç§»åŠ¨
     public bool ChangeTransform = true;
-    public wall wallPreFab;
+    public GameObject wallPrefab;
+    private GameObject wall;
+    public int extendLeft = 0;
+    public int extendRight = 0;
+    public int extendUp = 0;
+    public int extendDown = 0;
     void Start() {
+        wall = Instantiate(wallPrefab);
+        if (null == wall.GetComponent<wall>())
+        {
+            wall.AddComponent<wall>();
+        }
+        if (0 == wallXY.Length)
+        {
+            int begin = 0;
+            int end = 0;
+            int x = -1;
+            int y = -1;
+            char status = 'x';
+            List<Vector2> tempList = new List<Vector2>();
+            if (map.itemDelimiter != wallXYstr[wallXYstr.Length - 1])
+            {
+                wallXYstr += map.itemDelimiter;
+            }
+            for (int i = 0; i < wallXYstr.Length; i++)
+            {
+                if (map.itemDelimiter == wallXYstr[i] || map.xyDelimiter == wallXYstr[i])
+                {
+                    end = i;
+                    if ('x' == status)
+                    {
+                        x = int.Parse(wallXYstr.Substring(begin, end - begin));
+                        begin = end + 1;
+                        status = 'y';
+                    }
+                    else
+                    {
+                        y = int.Parse(wallXYstr.Substring(begin, end - begin));
+                        begin = end + 1;
+                        tempList.Add(new Vector2(x, y));
+                        status = 'x';
+                    }
+                }
+            }
+            wallXY = new Vector2[tempList.Count];
+            tempList.CopyTo(wallXY);
+        }
         if (wallXY.Length == 0)
         {
             return;
         }
         foreach (Vector2 xy in wallXY)
         {
-            if (null == wallPreFab)
+            if (null == wall)
             {
                 GameObject emptyWall = new GameObject("EmptyWall");
-                wallPreFab = emptyWall.AddComponent<wall>();
+                emptyWall.AddComponent<wall>();
+                wall = emptyWall;
             }
-            wallPreFab.x = (int)xy.x + (int)xyOffSet.x;
-            wallPreFab.y = (int)xy.y + (int)xyOffSet.y;
-            wallPreFab.ChangeTransform = ChangeTransform;
-            Instantiate(wallPreFab);
+            wall.GetComponent<wall>().x = (int)xy.x + (int)xyOffSet.x;
+            wall.GetComponent<wall>().y = (int)xy.y + (int)xyOffSet.y;
+            wall.GetComponent<wall>().ChangeTransform = ChangeTransform;
+            wall.GetComponent<wall>().extendUp = extendUp;
+            wall.GetComponent<wall>().extendLeft = extendLeft;
+            wall.GetComponent<wall>().extendDown = extendDown;
+            wall.GetComponent<wall>().extendRight = extendRight;
+            wall.GetComponent<wall>().wallPrefab = wallPrefab;
+            wall.GetComponent<wall>().m = m;
+            Instantiate(wall);
         }
     }
 
