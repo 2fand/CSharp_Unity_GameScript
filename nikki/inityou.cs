de0c2fe27656d48180d56b3c4743e3748402d2dd
2fand <167662034+2fand@users.noreply.github.com>
2025-07-16 19:42:57 +0800

    改名C#脚本inityou.cs

--- nikki/inityou.cs
+++ nikki/inityou.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class setyou : MonoBehaviour
+public class inityou : MonoBehaviour
 {
     public AudioClip defaultWalkSound;
     public AudioClip effectEqiupSound;
