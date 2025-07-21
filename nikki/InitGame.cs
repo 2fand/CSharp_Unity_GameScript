7b3d59d64f2637c5475ee7c4770d1a56eb588783
2fand <167662034+2fand@users.noreply.github.com>
2025-07-21 15:59:10 +0800

    更新C#脚本inityou.cs，并改名为InitGame.cs

--- nikki/InitGame.cs
+++ nikki/InitGame.cs
@@ -2,26 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class inityou : MonoBehaviour
+public class InitGame : MonoBehaviour
 {
     public AudioClip defaultWalkSound;
     public AudioClip effectEqiupSound;
     public AudioClip effectCancelEqiupSound;
-    public Material[] screens;
     public GameObject[] effects;
     public AudioClip[] effectWalkSounds;
+    public Font gameFont;
+    //额外的↓
+    public Material[] screens;
+    private static bool isInit = false;
+    public static bool IsInit
+    {
+        get
+        {
+            return isInit;
+        }
+    }
     void Awake()
     {
+        Game.gameFont = gameFont ?? Game.gameFont;
         you.defaultWalkSound = defaultWalkSound ?? you.defaultWalkSound;
         you.effectEqiupSound = effectEqiupSound ?? you.effectEqiupSound;
         you.effectCancelEqiupSound = effectCancelEqiupSound ?? you.effectCancelEqiupSound;
         you.screens = screens ?? you.screens;
         you.effects = effects ?? you.effects;
         you.effectWalkSounds = effectWalkSounds ?? you.effectWalkSounds;
+        isInit = true;
     }
 
     void Update()
     {
-        
+
     }
 }
