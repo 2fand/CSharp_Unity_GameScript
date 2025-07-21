86a6863c88979cc4913be0d35f24da24351dbeb7
2fand <167662034+2fand@users.noreply.github.com>
2025-07-21 16:00:01 +0800

    更新C#脚本fullscreen.cs，并改名为SetScreen.cs

---
+++ nikki/SetScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetScreen : MonoBehaviour
+{
+    public float h = 1;
+    public float w = 1;
+    public float x = 0.5f;
+    public float y = 0.5f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetComponent<Image>().rectTransform.position = new Vector2(Screen.width * x, Screen.height * y);
+        GetComponent<Image>().rectTransform.sizeDelta = new Vector2(Screen.width * w, Screen.height * h);
+    }
+}
