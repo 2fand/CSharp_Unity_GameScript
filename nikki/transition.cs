dbabbe7cf5a5d086332a8c3311f6fd93f01c8979
2fand <167662034+2fand@users.noreply.github.com>
2025-08-13 18:06:51 +0800

    更新C#脚本transition.cs

--- nikki/transition.cs
+++ nikki/transition.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using static change;
 
 public abstract class transition
 {
+    public abstract bool isExitTransition { get; }
+    public abstract enterMode transitionEnterMode { get; }
+    public abstract exitMode transitionExitMode { get; }
 #nullable enable
-    public static Hashtable transitions = new Hashtable { { enterMode.fadein, fadein.changeScene() }, { enterMode.show, show.changeScene() }, { exitMode.fadeout, fadeout.changeScene() }, { exitMode.hide, hide.changeScene() }, { enterMode.none, enterNone.changeScene() }, { exitMode.none, exitNone.changeScene() } };
+    public static Hashtable transitions = new Hashtable { { enterMode.fadein, new fadein().changeScene() }, { enterMode.show, new show().changeScene() }, { exitMode.fadeout, new fadeout().changeScene() }, { exitMode.hide, new hide().changeScene() }, { enterMode.none, new enterNone().changeScene() }, { exitMode.none, new exitNone().changeScene() } };
 #nullable disable
     protected static bool transitionIsEnd = true;
     public static bool TransitionIsEnd
@@ -17,14 +22,16 @@ public abstract class transition
             return transitionIsEnd;
         }
     }
-    public static bool showhasTest
+    public abstract IEnumerator changeScene(float time = 0.1f);
+    static transition(){
+        init();
+    }
+    private static void init()
     {
-        get {
-            return transitions.ContainsKey(enterMode.show);
-        }
+        transitions = new Hashtable { { enterMode.fadein, new fadein().changeScene() }, { enterMode.show, new show().changeScene() }, { exitMode.fadeout, new fadeout().changeScene() }, { exitMode.hide, new hide().changeScene() }, { enterMode.none, new enterNone().changeScene() }, { exitMode.none, new exitNone().changeScene() } };
     }
     public static void resetTransitions()
     {
-        transitions = new Hashtable { { enterMode.fadein, fadein.changeScene() }, { enterMode.show, show.changeScene() }, { exitMode.fadeout, fadeout.changeScene() }, { exitMode.hide, hide.changeScene() }, { enterMode.none, enterNone.changeScene() }, { exitMode.none, exitNone.changeScene() } };
+        init();
     }
 }
