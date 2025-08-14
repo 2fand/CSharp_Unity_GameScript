75ad4c3d0c5cbec2844012bd084e51324420ff6c
2fand <167662034+2fand@users.noreply.github.com>
2025-08-14 19:14:00 +0800

    更新C#脚本transition.cs

--- nikki/transition.cs
+++ nikki/transition.cs
@@ -8,11 +8,9 @@ using static change;
 
 public abstract class transition
 {
-    public abstract bool isExitTransition { get; }
-    public abstract enterMode transitionEnterMode { get; }
-    public abstract exitMode transitionExitMode { get; }
+    public abstract transitionMode _transitionMode { get; }
 #nullable enable
-    public static Hashtable transitions = new Hashtable { { enterMode.fadein, new fadein().changeScene() }, { enterMode.show, new show().changeScene() }, { exitMode.fadeout, new fadeout().changeScene() }, { exitMode.hide, new hide().changeScene() }, { enterMode.none, new enterNone().changeScene() }, { exitMode.none, new exitNone().changeScene() } };
+    public static Hashtable transitions;
 #nullable disable
     protected static bool transitionIsEnd = true;
     public static bool TransitionIsEnd
@@ -28,7 +26,7 @@ public abstract class transition
     }
     private static void init()
     {
-        transitions = new Hashtable { { enterMode.fadein, new fadein().changeScene() }, { enterMode.show, new show().changeScene() }, { exitMode.fadeout, new fadeout().changeScene() }, { exitMode.hide, new hide().changeScene() }, { enterMode.none, new enterNone().changeScene() }, { exitMode.none, new exitNone().changeScene() } };
+        transitions = new Hashtable { { transitionMode.fadein, new fadein().changeScene() }, { transitionMode.show, new show().changeScene() }, { transitionMode.fadeout, new fadeout().changeScene() }, { transitionMode.hide, new hide().changeScene() }, { transitionMode.enterNone, new enterNone().changeScene() }, { transitionMode.exitNone, new exitNone().changeScene() } };
     }
     public static void resetTransitions()
     {
